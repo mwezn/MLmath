321b96f4a196537a11a14983cc1be9ea9c89492e
mwezn <m.w.g.nelson@gmail.com>
2023-08-06 09:05:03 +0100

    display each number in png buffer

--- imgs/server.js
+++ imgs/server.js
@@ -1,18 +1,19 @@
 const express = require('express');
-const PNG= require('png-js')
+const fs= require('fs')
 
 const server= express();
 
-PNG.decode('./dcode1.png', function(pixels) {
-    // Pixels is a 1d array (in rgba order) of decoded pixel data
-    for (let i=0;i<pixels.length;i++){
-        console.log(pixels[i])
-        
-    }
-    console.log(pixels.length,pixels.length/40)
- });
+const pngFile= fs.readFileSync('./dcode1.png')
 
+ console.log(pngFile)
 
+server.get('/', (req, res)=>{
+    res.sendFile('./pixels.html',{root: '../imgs'})
+})
+
+server.get('/pixels', (req, res)=>{
+    res.json(pngFile)
+})
 
 server.listen(3000,()=>{
     console.log('Server listening on port 3000:')
