f65b5bfb96a1fc3dec1620dc88971ff40578f49c
mwezn <m.w.g.nelson@gmail.com>
2023-11-12 07:42:22 +0000

    enlarged image pixels on hover

--- server.js
+++ server.js
@@ -11,9 +11,23 @@ let firstLine=results[7].split(',')
 let final=firstLine.slice(1).map(d=>Number(d))
 console.log(firstLine,final)
 
+app.use(express.static(__dirname));
 app.get('/', (req,res)=>{
     res.sendFile('./render.html',{root: __dirname})
 })
+
+app.get('/words', (req, res)=>{
+    res.sendFile('./worddisplay.html', {root: __dirname})
+})
+
+app.get('/paint', (req, res)=>{
+    res.sendFile('./paint.html', {root: __dirname})
+})
+
+app.get('/steg', (req, res)=>{
+    res.sendFile('./steg.html', {root: __dirname})
+})
+
 
 app.get('/data', (req, res)=>{
     res.send(final)
